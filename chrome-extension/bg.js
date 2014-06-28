57a83f9fbd90773a44bda9001b23940b81200143
Arnoldnuo <arnoldnuo@gmail.com>
2014-06-28 16:03:07 +0800

    hell

--- chrome-extension/bg.js
+++ chrome-extension/bg.js
@@ -2,3 +2,7 @@ chrome.browserAction.onClicked.addListener(function(tab){
   chrome.tabs.create({url:'view.html'}); 
 });
 
+chrome.windows.onCreated.addListener(function(window){
+  console.log(window);
+});
+
