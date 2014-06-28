4054d5e4a6d75f726e143d2c2bd5bde574fa5926
Yang <zhy216@gmail.com>
2014-06-28 03:18:11 -0400

    add two fake api

--- xtimr/urls.py
+++ xtimr/urls.py
@@ -5,6 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
+    url(r'^get-user-type$', 'web.views.fake_get_user_type'),
+    url(r'^get-browse-datetime$', 'web.views.fake_get_browse_datetime'),
     # url(r'^$', 'xtimr.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
